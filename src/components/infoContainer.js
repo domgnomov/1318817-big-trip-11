b41a5f2a2eafdc9547fef1e3c1146ea01a3a9c55
nivanov <nivanov@phoenixit.ru>
2020-04-04 10:26:19 +0300

    Личный проект: время разбивать камни

---
+++ src/components/infoContainer.js
@@ -0,0 +1,9 @@
+export const createInfoContainerTemplate = () => {
+  return (
+    `<section class="trip-main__trip-info  trip-info">
+      <div class="trip-info__main"></div>
+      
+      <p class="trip-info__cost"></p>
+    </section>`
+  );
+};
